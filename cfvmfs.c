12a5f2b2b6376a0b5a1a9950f41edb6511b25682
Christophe Fillot <christophe.fillot@utc.fr>
2009-04-16 19:23:58 +0200

    Added a bitmap check procedure to detect inconsistencies in the bitmap meta-files.

--- cfvmfs.c
+++ cfvmfs.c
@@ -139,6 +139,8 @@ struct vmfs_bitmap_header {
 /* === Bitmap entry === */
 #define VMFS_BITMAP_ENTRY_SIZE  0x400
 
+#define VMFS_BME_OFS_BITMAP     0x210
+
 struct vmfs_bitmap_entry {
    m_u32_t magic;
    m_u32_t position;
@@ -306,6 +308,17 @@ void mem_dump(FILE *f_output,u_char *pkt,u_int len)
    fflush(f_output);
 }
 
+/* Count the number of bits set in a byte */
+int bit_count(u_char val)
+{
+   static int qb[16] = {
+      0, 1, 1, 2, 1, 2, 2, 3,
+      1, 2, 2, 3, 2, 3, 3, 4,
+   };
+
+   return(qb[val >> 4] + qb[val & 0x0F]);
+}
+
 /* ======================================================================== */
 /* Marshalling                                                              */
 /* ======================================================================== */
@@ -438,7 +451,7 @@ int vmfs_bme_read(vmfs_bitmap_entry_t *bme,u_char *buf,int copy_bitmap)
    bme->alloc    = read_le32(buf,0x20c);
 
    if (copy_bitmap) {
-      memcpy(bme->bitmap,&buf[0x210],(bme->total+7)/8);
+      memcpy(bme->bitmap,&buf[VMFS_BME_OFS_BITMAP ],(bme->total+7)/8);
    }
 
    return(0);
@@ -545,6 +558,89 @@ m_u32_t vmfs_bitmap_allocated_items(vmfs_file_t *f,vmfs_bitmap_header_t *bmh)
       count += vmfs_bitmap_area_allocated_items(f,bmh,i);
 
    return(count);
+}
+
+/* Check coherency of a bitmap file */
+int vmfs_bitmap_check(vmfs_file_t *f,vmfs_bitmap_header_t *bmh)
+{  
+   u_char buf[VMFS_BITMAP_ENTRY_SIZE];
+   vmfs_bitmap_entry_t entry;
+   m_u32_t total_items;
+   m_u32_t magic;
+   m_u32_t entry_id;
+   int i,j,k,errors;
+   int bmap_size;
+   int bmap_count;
+
+   errors      = 0;
+   total_items = 0;
+   magic       = 0;
+   entry_id    = 0;
+
+   for(i=0;i<bmh->area_count;i++) {
+      vmfs_file_seek(f,vmfs_bitmap_get_area_addr(bmh,i),SEEK_SET);
+
+      for(j=0;j<bmh->bmp_entries_per_area;j++) {
+         if (vmfs_file_read(f,buf,sizeof(buf)) != sizeof(buf))
+            break;
+
+         vmfs_bme_read(&entry,buf,0);
+
+         if (entry.magic == 0)
+            goto done;
+
+         /* check the entry ID */
+         if (entry.id != entry_id) {
+            printf("Entry 0x%x has incorrect ID 0x%x\n",entry_id,entry.id);
+            errors++;
+         }
+         
+         /* check the magic number */
+         if (magic == 0) {
+            magic = entry.magic;
+         } else {
+            if (entry.magic != magic) {
+               printf("Entry 0x%x has an incorrect magic id (0x%x)\n",
+                      entry_id,entry.magic);
+               errors++;
+            }
+         }
+         
+         /* check the number of items */
+         if (entry.total > bmh->items_per_bitmap_entry) {
+            printf("Entry 0x%x has an incorrect total of 0x%2.2x items\n",
+                   entry_id,entry.total);
+            errors++;
+         }
+
+         /* check the bitmap array */
+         bmap_size = (entry.total + 7) / 8;
+         bmap_count = 0;
+
+         for(k=0;k<bmap_size;k++) {
+            bmap_count += bit_count(buf[VMFS_BME_OFS_BITMAP+k]);
+         }
+
+         if (bmap_count != entry.free) {
+            printf("Entry 0x%x has an incorrect bitmap array "
+                   "(bmap_count=0x%x instead of 0x%x)\n",
+                   entry_id,bmap_count,entry.alloc);
+            errors++;
+         }
+
+         total_items += entry.total;
+         entry_id++;
+      }
+   }
+
+ done:
+   if (total_items != bmh->total_items) {
+      printf("Total number of items (0x%x) doesn't match header info (0x%x)\n",
+             total_items,bmh->total_items);
+      errors++;
+   }
+
+   return(errors);
 }
 
 /* ======================================================================== */
@@ -1282,15 +1378,20 @@ int main(int argc,char *argv[])
       
       count = vmfs_bitmap_allocated_items(vol->fbb,&vol->fbb_bmh);
       printf("FBB allocated items: %d (0x%x)\n",count,count);
+      vmfs_bitmap_check(vol->fbb,&vol->fbb_bmh);
 
       count = vmfs_bitmap_allocated_items(vol->fdc,&vol->fdc_bmh);
       printf("FDC allocated items: %d (0x%x)\n",count,count);
+      vmfs_bitmap_check(vol->fdc,&vol->fdc_bmh);
 
       count = vmfs_bitmap_allocated_items(vol->pbc,&vol->pbc_bmh);
       printf("PBC allocated items: %d (0x%x)\n",count,count);
+      vmfs_bitmap_check(vol->pbc,&vol->pbc_bmh);
 
       count = vmfs_bitmap_allocated_items(vol->sbc,&vol->sbc_bmh);
       printf("SBC allocated items: %d (0x%x)\n",count,count);
+      vmfs_bitmap_check(vol->sbc,&vol->sbc_bmh);
+
    }
 #endif
 
