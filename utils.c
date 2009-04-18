b41f131dc1db8b0d2207634aa6a03104258c0ae1
Christophe Fillot <cf@utc.fr>
2009-04-18 10:59:15 +0200

    Move utility functions into specific files (utils.[ch])

---
+++ utils.c
@@ -0,0 +1,66 @@
+/*
+ * Utility functions.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <uuid/uuid.h>
+
+#include "utils.h"
+
+/* Convert an UUID into a string (TODO: respect VMware format ?) */
+char *m_uuid_to_str(uuid_t uuid,char *str)
+{
+   uuid_unparse(uuid, str);
+   return str;
+}
+
+/* Dump a structure in hexa and ascii */
+void mem_dump(FILE *f_output,u_char *pkt,u_int len)
+{
+   u_int x,i = 0, tmp;
+
+   while (i < len)
+   {
+      if ((len - i) > 16)
+         x = 16;
+      else x = len - i;
+
+      fprintf(f_output,"%4.4x: ",i);
+
+      for (tmp=0;tmp<x;tmp++)
+         fprintf(f_output,"%2.2x ",pkt[i+tmp]);
+      for (tmp=x;tmp<16;tmp++) fprintf(f_output,"   ");
+
+      for (tmp=0;tmp<x;tmp++) {
+         char c = pkt[i+tmp];
+
+         if (((c >= 'A') && (c <= 'Z')) ||
+             ((c >= 'a') && (c <= 'z')) ||
+             ((c >= '0') && (c <= '9')))
+            fprintf(f_output,"%c",c);
+         else
+            fputs(".",f_output);
+      }
+
+      i += x;
+      fprintf(f_output,"\n");
+   }
+
+   fprintf(f_output,"\n");
+   fflush(f_output);
+}
+
+/* Count the number of bits set in a byte */
+int bit_count(u_char val)
+{
+   static int qb[16] = {
+      0, 1, 1, 2, 1, 2, 2, 3,
+      1, 2, 2, 3, 2, 3, 3, 4,
+   };
+
+   return(qb[val >> 4] + qb[val & 0x0F]);
+}
---
+++ utils.h
@@ -0,0 +1,48 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <sys/types.h>
+
+/* Max and min macro */
+#define m_max(a,b) (((a) > (b)) ? (a) : (b))
+#define m_min(a,b) (((a) < (b)) ? (a) : (b))
+
+#define M_UUID_BUFLEN  128
+
+typedef unsigned char m_u8_t;
+typedef unsigned short m_u16_t;
+typedef unsigned int m_u32_t;
+typedef unsigned long long m_u64_t;
+
+/* Read a 16-bit word in little endian format */
+static inline m_u16_t read_le16(u_char *p,int offset)
+{
+   return((m_u16_t)p[offset] | ((m_u16_t)p[offset+1] << 8));
+}
+
+/* Read a 32-bit word in little endian format */
+static inline m_u32_t read_le32(u_char *p,int offset)
+{
+   return((m_u32_t)p[offset] |
+          ((m_u32_t)p[offset+1] << 8) |
+          ((m_u32_t)p[offset+2] << 16) |
+          ((m_u32_t)p[offset+3] << 24));
+}
+
+/* Read a 64-bit word in little endian format */
+static inline m_u64_t read_le64(u_char *p,int offset)
+{
+   return((m_u64_t)read_le32(p,offset) + 
+          ((m_u64_t)read_le32(p,offset+4) << 32));
+}
+
+/* Convert an UUID into a string */
+char *m_uuid_to_str(uuid_t uuid,char *str);
+
+/* Dump a structure in hexa and ascii */
+void mem_dump(FILE *f_output,u_char *pkt,u_int len);
+
+/* Count the number of bits set in a byte */
+int bit_count(u_char val);
+
+#endif
