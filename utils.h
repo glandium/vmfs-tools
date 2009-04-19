f5316a65ca352d0834109259f8bc86a6c84158d7
Mike Hommey <mh@glandium.org>
2009-04-19 13:05:38 +0200

    Read directly from buffer in read_le{16,32,64} when the architecture permits

--- utils.h
+++ utils.h
@@ -22,26 +22,42 @@ typedef unsigned short m_u16_t;
 typedef unsigned int m_u32_t;
 typedef unsigned long long m_u64_t;
 
+#if defined(__amd64__) || defined(__i386__)
+#define LE_AND_NO_ALIGN 1
+#endif
+
 /* Read a 16-bit word in little endian format */
 static inline m_u16_t read_le16(u_char *p,int offset)
 {
+#ifdef LE_AND_NO_ALIGN
+   return((m_u16_t) *((m_u16_t *)&p[offset]));
+#else
    return((m_u16_t)p[offset] | ((m_u16_t)p[offset+1] << 8));
+#endif
 }
 
 /* Read a 32-bit word in little endian format */
 static inline m_u32_t read_le32(u_char *p,int offset)
 {
+#ifdef LE_AND_NO_ALIGN
+   return((m_u32_t) *((m_u32_t *)&p[offset]));
+#else
    return((m_u32_t)p[offset] |
           ((m_u32_t)p[offset+1] << 8) |
           ((m_u32_t)p[offset+2] << 16) |
           ((m_u32_t)p[offset+3] << 24));
+#endif
 }
 
 /* Read a 64-bit word in little endian format */
 static inline m_u64_t read_le64(u_char *p,int offset)
 {
+#ifdef LE_AND_NO_ALIGN
+   return((m_u64_t) *((m_u64_t *)&p[offset]));
+#else
    return((m_u64_t)read_le32(p,offset) + 
           ((m_u64_t)read_le32(p,offset+4) << 32));
+#endif
 }
 
 /* Convert an UUID into a string */
