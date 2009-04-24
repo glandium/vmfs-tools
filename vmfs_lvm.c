04b82cc5a9cc1f415024297e2af22a023199b00f
Mike Hommey <mh@glandium.org>
2009-04-24 21:48:03 +0200

    Check if LVM info from added extent matches that of the first extent

--- vmfs_lvm.c
+++ vmfs_lvm.c
@@ -69,6 +69,12 @@ int vmfs_lvm_add_extent(vmfs_lvm_t *lvm, char *filename)
    } else if (uuid_compare(lvm->lvm_info.uuid, vol->vol_info.lvm_uuid)) {
       fprintf(stderr, "VMFS: The %s file/device is not part of the LVM\n", filename);
       return(-1);
+   } else if ((lvm->lvm_info.size != vol->vol_info.size) ||
+              (lvm->lvm_info.blocks != vol->vol_info.blocks) ||
+              (lvm->lvm_info.num_extents != vol->vol_info.num_extents)) {
+      fprintf(stderr, "VMFS: LVM information mismatch for the %s"
+                      " file/device\n", filename);
+      return(-1);
    }
 
    lvm->extents[lvm->loaded_extents++] = vol;
