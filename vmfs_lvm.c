3f828af845184fe431fbd2e48bf4970f2ac712d6
Christophe Fillot <cf@utc.fr>
2009-04-25 08:07:03 +0200

    Add basic LVM write support

--- vmfs_lvm.c
+++ vmfs_lvm.c
@@ -7,35 +7,65 @@
 
 #define VMFS_LVM_SEGMENT_SIZE (256 * 1024 * 1024)
 
-/* Until we uncover the details of the segment descriptors format,
-   it is useless to try to do something more efficient */
+/* 
+ * Until we uncover the details of the segment descriptors format,
+ * it is useless to try to do something more efficient.
+ */
 
 static int vmfs_lvm_get_extent_from_offset(vmfs_lvm_t *lvm,off_t pos)
 {
    int extent;
    off_t segment = pos / VMFS_LVM_SEGMENT_SIZE;
+
    for (extent = 0; extent < lvm->loaded_extents; extent++) {
       if ((segment >= lvm->extents[extent]->vol_info.first_segment) &&
           (segment <= lvm->extents[extent]->vol_info.last_segment))
         return(extent);
    }
+
    return(-1);
+}
+
+/* Get extent size */
+static inline ssize_t vmfs_lvm_extent_size(vmfs_lvm_t *lvm,int extent)
+{
+   return(lvm->extents[extent]->vol_info.num_segments * VMFS_LVM_SEGMENT_SIZE);
 }
 
 /* Read a raw block of data on logical volume */
 ssize_t vmfs_lvm_read(vmfs_lvm_t *lvm,off_t pos,u_char *buf,size_t len)
 {
    int extent = vmfs_lvm_get_extent_from_offset(lvm,pos);
+
    if (extent < 0)
       return(-1);
+
    pos -= lvm->extents[extent]->vol_info.first_segment * VMFS_LVM_SEGMENT_SIZE;
-   if (pos + len > lvm->extents[extent]->vol_info.num_segments * VMFS_LVM_SEGMENT_SIZE) {
+   if ((pos + len) > vmfs_lvm_extent_size(lvm,extent)) {
       /* TODO: Handle this case */
       fprintf(stderr,"VMFS: i/o spanned over several extents is unsupported\n");
       return(-1);
    }
 
    return(vmfs_vol_read(lvm->extents[extent],pos,buf,len));
+}
+
+/* Write a raw block of data on logical volume */
+ssize_t vmfs_lvm_write(vmfs_lvm_t *lvm,off_t pos,u_char *buf,size_t len)
+{
+   int extent = vmfs_lvm_get_extent_from_offset(lvm,pos);
+
+   if (extent < 0)
+      return(-1);
+
+   pos -= lvm->extents[extent]->vol_info.first_segment * VMFS_LVM_SEGMENT_SIZE;
+   if ((pos + len) > vmfs_lvm_extent_size(lvm,extent)) {
+      /* TODO: Handle this case */
+      fprintf(stderr,"VMFS: i/o spanned over several extents is unsupported\n");
+      return(-1);
+   }
+
+   return(vmfs_vol_write(lvm->extents[extent],pos,buf,len));
 }
 
 /* Show lvm information */
--- vmfs_lvm.h
+++ vmfs_lvm.h
@@ -27,6 +27,9 @@ struct vmfs_lvm {
 /* Read a raw block of data on logical volume */
 ssize_t vmfs_lvm_read(vmfs_lvm_t *lvm,off_t pos,u_char *buf,size_t len);
 
+/* Read a raw block of data on logical volume */
+ssize_t vmfs_lvm_read(vmfs_lvm_t *lvm,off_t pos,u_char *buf,size_t len);
+
 /* Show lvm information */
 void vmfs_lvm_show(vmfs_lvm_t *lvm);
 
