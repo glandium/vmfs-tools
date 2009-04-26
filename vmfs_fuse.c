ac87168eb3013882962188ac9756772cf65c4bcc
Mike Hommey <mh@glandium.org>
2009-04-26 22:15:33 +0200

    Open the VMFS in vmfs_fuse

--- vmfs_fuse.c
+++ vmfs_fuse.c
@@ -3,6 +3,7 @@
 #include <fuse.h>
 #include <stdlib.h>
 #include <string.h>
+#include "vmfs.h"
 
 const static struct fuse_operations vmfs_oper = {
 };
@@ -29,6 +30,8 @@ int main(int argc, char *argv[])
 {
    struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
    struct vmfs_fuse_opts opts = { 0, };
+   vmfs_fs_t *fs;
+   vmfs_lvm_t *lvm;
    int err = -1;
 
    if ((fuse_opt_parse(&args, &opts, NULL, &vmfs_fuse_opts_func) == -1) ||
@@ -37,7 +40,27 @@ int main(int argc, char *argv[])
       goto cleanup;
    }
 
-   err = fuse_main(args.argc, args.argv, &vmfs_oper, NULL);
+   if (!(lvm = vmfs_lvm_create(0))) {
+      fprintf(stderr,"Unable to create LVM structure\n");
+      goto cleanup;
+   }
+
+   if (vmfs_lvm_add_extent(lvm,opts.dev) == -1) {
+      fprintf(stderr,"Unable to open device/file \"%s\".\n",opts.dev);
+      goto cleanup;
+   }
+
+   if (!(fs = vmfs_fs_create(lvm,0))) {
+      fprintf(stderr,"Unable to open filesystem\n");
+      goto cleanup;
+   }
+
+   if (vmfs_fs_open(fs) == -1) {
+      fprintf(stderr,"Unable to open volume.\n");
+      goto cleanup;
+   }
+
+   err = fuse_main(args.argc, args.argv, &vmfs_oper, fs);
 
 cleanup:
    fuse_opt_free_args(&args);
