2f840bb3ee70b2f873cfe39efe0322f44ae527a8
Christophe Fillot <cf@utc.fr>
2009-06-03 19:01:23 +0200

    Add recursive walk through directories

--- vmfs_fsck.c
+++ vmfs_fsck.c
@@ -49,6 +49,15 @@ struct vmfs_blk_map {
    vmfs_blk_map_t *prev,*next;
 };
 
+typedef struct vmfs_fsck_info vmfs_fsck_info_t;
+struct vmfs_fsck_info {
+   vmfs_blk_map_t *blk_map[VMFS_BLK_MAP_BUCKETS];
+   u_int blk_count[VMFS_BLK_TYPE_MAX];
+
+   /* Inodes referenced in directory structure but not in FDC */
+   u_int undef_inodes;
+};
+
 /* Hash function for a block ID */
 static inline u_int vmfs_block_map_hash(uint32_t blk_id)
 {
@@ -127,18 +136,15 @@ static int vmfs_fsck_store_inode(const vmfs_fs_t *fs,vmfs_blk_map_t **ht,
 }
 
 /* Iterate over all inodes of the FS and get all block mappings  */
-int vmfs_fsck_get_all_block_mappings(const vmfs_fs_t *fs)
+int vmfs_fsck_get_all_block_mappings(const vmfs_fs_t *fs,
+                                     vmfs_fsck_info_t *fi)
 {
    DECL_ALIGNED_BUFFER_WOL(inode_buf,VMFS_INODE_SIZE);
    vmfs_inode_t inode;
    vmfs_bitmap_header_t *fdc_bmp;
-   vmfs_blk_map_t *ht[VMFS_BLK_MAP_BUCKETS];
-   vmfs_blk_map_t *map;
    uint32_t entry,item;
-   u_int blk_count[VMFS_BLK_TYPE_MAX] = { 0, };
    int i;
 
-   memset(ht,0,sizeof(ht));
    fdc_bmp = &fs->fdc->bmh;
 
    printf("Scanning %u FDC entries...\n",fdc_bmp->total_items);
@@ -156,30 +162,106 @@ int vmfs_fsck_get_all_block_mappings(const vmfs_fs_t *fs)
       if ((vmfs_inode_read(&inode,inode_buf) == -1) || !inode.nlink)
          continue;
 
-      vmfs_fsck_store_inode(fs,ht,&inode);
-      vmfs_inode_foreach_block(fs,&inode,vmfs_fsck_store_block,ht);
+      vmfs_fsck_store_inode(fs,fi->blk_map,&inode);
+      vmfs_inode_foreach_block(fs,&inode,vmfs_fsck_store_block,fi->blk_map);
    }
 
-   for(i=0;i<VMFS_BLK_MAP_BUCKETS;i++)
-      for(map=ht[i];map;map=map->next) {
-         u_int blk_type;
+   return(0);
+}
 
-         if (map->ref_count > 1) {
-            fprintf(stderr,"Block 0x%8.8x has multiple references\n",
-                    map->blk_id);
+/* Display Inode IDs for blocks incorrectly shared by multiple inodes */
+void vmfs_fsck_show_inode_id(vmfs_blk_map_t *map)
+{
+   u_int i,inode_count;
+
+   inode_count = m_min(map->ref_count,VMFS_BLK_MAP_MAX_INODES);
+
+   for(i=0;i<inode_count;i++)
+      printf("0x%8.8x ",map->inode_id[i]);
+
+   printf("\n");
+}
+
+/* Count block types */
+void vmfs_fsck_count_blocks(vmfs_fsck_info_t *fi)
+{
+   vmfs_blk_map_t *map;
+   u_int blk_type;
+   int i;
+
+   for(i=0;i<VMFS_BLK_MAP_BUCKETS;i++) {
+      for(map=fi->blk_map[i];map;map=map->next) {
+         blk_type = VMFS_BLK_TYPE(map->blk_id);
+
+         if ((blk_type != VMFS_BLK_TYPE_FD) && (map->ref_count > 1)) {
+            printf("Block 0x%8.8x is referenced by multiple inodes: \n",
+                   map->blk_id);
+            vmfs_fsck_show_inode_id(map);
          }
 
-         blk_type = VMFS_BLK_TYPE(map->blk_id);
          if (blk_type < VMFS_BLK_TYPE_MAX)
-            blk_count[blk_type]++;
+            fi->blk_count[blk_type]++;
+      }
+   }
+   
+   printf("Data collected from inode entries:\n");   
+   printf("  File Blocks    : %u\n",fi->blk_count[VMFS_BLK_TYPE_FB]);
+   printf("  Sub-Blocks     : %u\n",fi->blk_count[VMFS_BLK_TYPE_SB]);
+   printf("  Pointer Blocks : %u\n",fi->blk_count[VMFS_BLK_TYPE_PB]);
+   printf("  Inodes         : %u\n\n",fi->blk_count[VMFS_BLK_TYPE_FD]);
+}
+
+/* 
+ * Walk recursively through the directory structure and check inode 
+ * allocation.
+ */
+int vmfs_fsck_walk_dir(const vmfs_fs_t *fs,
+                       vmfs_fsck_info_t *fi,
+                       vmfs_file_t *dir_entry)
+{   
+   u_char buf[VMFS_DIRENT_SIZE];
+   vmfs_dirent_t rec;
+   vmfs_file_t *sub_dir;
+   vmfs_blk_map_t *map;
+   int i,res,dir_count;
+   ssize_t len;
+
+   dir_count = vmfs_file_get_size(dir_entry) / VMFS_DIRENT_SIZE;
+   vmfs_file_seek(dir_entry,0,SEEK_SET);
+
+   for(i=0;i<dir_count;i++) {
+      len = vmfs_file_read(dir_entry,buf,sizeof(buf));
+      
+      if (len != sizeof(buf))
+         return(-1);
+
+      vmfs_dirent_read(&rec,buf);
+
+      if (rec.type == VMFS_FILE_TYPE_DIR) {
+         if (strcmp(rec.name,".") && strcmp(rec.name,"..")) {
+            if (!(sub_dir = vmfs_file_open_from_rec(fs,&rec)))
+               return(-1);
+
+            res = vmfs_fsck_walk_dir(fs,fi,sub_dir);
+            vmfs_file_close(sub_dir);
+         
+            if (res == -1)
+               return(-1);
+         }
       }
 
-   printf("File Blocks    : %u\n",blk_count[VMFS_BLK_TYPE_FB]);
-   printf("Sub-Blocks     : %u\n",blk_count[VMFS_BLK_TYPE_SB]);
-   printf("Pointer Blocks : %u\n",blk_count[VMFS_BLK_TYPE_PB]);
-   printf("Inodes         : %u\n",blk_count[VMFS_BLK_TYPE_FD]);
+      if (!(map = vmfs_block_map_find(fi->blk_map,rec.block_id))) {
+         fi->undef_inodes++;
+      }
+   }
 
    return(0);
+}
+
+/* Initialize fsck structures */
+static void vmfs_fsck_init(vmfs_fsck_info_t *fi)
+{
+   memset(fi,0,sizeof(*fi));
 }
 
 static void show_usage(char *prog_name) 
@@ -192,6 +274,7 @@ static void show_usage(char *prog_name)
 
 int main(int argc,char *argv[])
 {
+   vmfs_fsck_info_t fsck_info;
    vmfs_lvm_t *lvm;
    vmfs_fs_t *fs;
    vmfs_flags_t flags;
@@ -226,7 +309,13 @@ int main(int argc,char *argv[])
       exit(EXIT_FAILURE);
    }
 
-   vmfs_fsck_get_all_block_mappings(fs);
+   vmfs_fsck_init(&fsck_info);
+   vmfs_fsck_get_all_block_mappings(fs,&fsck_info);
+   vmfs_fsck_walk_dir(fs,&fsck_info,fs->root_dir);
+
+   vmfs_fsck_count_blocks(&fsck_info);
+
+   printf("Undefined inodes: %u\n",fsck_info.undef_inodes);
 
    vmfs_fs_close(fs);
    return(0);
