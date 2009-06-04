3d42750c9cc73c99442dc741191f230df3177ee6
Christophe Fillot <cf@utc.fr>
2009-06-04 15:07:20 +0200

    Use defines for block items/entries shift and mask values

--- vmfs_block.h
+++ vmfs_block.h
@@ -32,20 +32,47 @@ enum {
 #define VMFS_BLK_TYPE(blk_id)  ((blk_id) & 0x07)
 
 /* File-Block - TBZ flag specifies if the block must be zeroed. */
-#define VMFS_BLK_FB_ITEM(blk_id)   ((blk_id) >> 6)
-#define VMFS_BLK_FB_TBZ(blk_id)    (((blk_id) >> 5) & 0x1)
+#define VMFS_BLK_FB_ITEM_SHIFT  6
+#define VMFS_BLK_FB_TBZ_SHIFT   5
+
+#define VMFS_BLK_FB_ITEM(blk_id)   ((blk_id) >> VMFS_BLK_FB_ITEM_SHIFT)
+#define VMFS_BLK_FB_TBZ(blk_id)    (((blk_id) >> VMFS_BLK_FB_TBZ_SHIFT) & 0x1)
 
 /* Sub-Block */
-#define VMFS_BLK_SB_ITEM(blk_id)   (((blk_id) >> 28) & 0x0f)
-#define VMFS_BLK_SB_ENTRY(blk_id)  (((blk_id) >> 6)  & 0x3fffff)
+#define VMFS_BLK_SB_ITEM_SHIFT   28
+#define VMFS_BLK_SB_ITEM_MASK    0x0f
+#define VMFS_BLK_SB_ENTRY_SHIFT  6
+#define VMFS_BLK_SB_ENTRY_MASK   0x3fffff
+
+#define VMFS_BLK_SB_ITEM(blk_id) \
+   (((blk_id) >> VMFS_BLK_SB_ITEM_SHIFT) & VMFS_BLK_SB_ITEM_MASK)
+
+#define VMFS_BLK_SB_ENTRY(blk_id) \
+   (((blk_id) >> VMFS_BLK_SB_ENTRY_SHIFT) & VMFS_BLK_SB_ENTRY_MASK)
 
 /* Pointer-Block */
-#define VMFS_BLK_PB_ITEM(blk_id)   (((blk_id) >> 28) & 0x0f)
-#define VMFS_BLK_PB_ENTRY(blk_id)  (((blk_id) >> 6)  & 0x3fffff)
+#define VMFS_BLK_PB_ITEM_SHIFT   28
+#define VMFS_BLK_PB_ITEM_MASK    0x0f
+#define VMFS_BLK_PB_ENTRY_SHIFT  6
+#define VMFS_BLK_PB_ENTRY_MASK   0x3fffff
+
+#define VMFS_BLK_PB_ITEM(blk_id) \
+   (((blk_id) >> VMFS_BLK_PB_ITEM_SHIFT) & VMFS_BLK_PB_ITEM_MASK)
+
+#define VMFS_BLK_PB_ENTRY(blk_id) \
+   (((blk_id) >> VMFS_BLK_PB_ENTRY_SHIFT) & VMFS_BLK_PB_ENTRY_MASK)
 
 /* File Descriptor */
-#define VMFS_BLK_FD_ITEM(blk_id)   (((blk_id) >> 22) & 0x3ff)
-#define VMFS_BLK_FD_ENTRY(blk_id)  (((blk_id) >> 6)  & 0x7fff)
+#define VMFS_BLK_FD_ITEM_SHIFT   22
+#define VMFS_BLK_FD_ITEM_MASK    0x3ff
+#define VMFS_BLK_FD_ENTRY_SHIFT  6
+#define VMFS_BLK_FD_ENTRY_MASK   0x7fff
+
+#define VMFS_BLK_FD_ITEM(blk_id) \
+   (((blk_id) >> VMFS_BLK_FD_ITEM_SHIFT) & VMFS_BLK_FD_ITEM_MASK)
+
+#define VMFS_BLK_FD_ENTRY(blk_id) \
+   (((blk_id) >> VMFS_BLK_FD_ENTRY_SHIFT) & VMFS_BLK_FD_ENTRY_MASK)
 
 /* Get bitmap info (bitmap pointer,entry and item) from a block ID */
 int vmfs_block_get_bitmap_info(const vmfs_fs_t *fs,uint32_t blk_id,
